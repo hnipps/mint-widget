f4a936beb8aacf69b2cf922e85c6e8e3c6d29a68
Harry Nicholls <harry@goldenx.team>
2021-12-21 10:31:19 -0500

    Add window function

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,23 @@ import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import theme from "./theme";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById("mint-controls")
-);
+declare global {
+  interface Window {
+    mintWidgetInit: () => void;
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const init = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>,
+    document.getElementById("mint-controls")
+  );
+};
+
+window.mintWidgetInit = init;
